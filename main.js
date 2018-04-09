1f4af6f3fd0c6c4612ceba2cf917d769264e353e
Abdurauf <abduraufcoder@gmail.com>
2018-04-09 19:22:54 +0500

    http r
    sada

--- main.js
+++ main.js
@@ -5,11 +5,7 @@ var io = require("socket.io")({
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 
-var http = require("http");
 
-http.createServer(function(req, res){
-	res.end("TEST succesfful");
-}).listen(server_port);
 
 io.attach(server_port);
 
