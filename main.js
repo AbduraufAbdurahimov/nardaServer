f4143937c5f242f6e7cf7c9def0ffa7d8fe3e00a
Abdurauf <abduraufcoder@gmail.com>
2018-04-09 19:03:16 +0500

    asdf
    adf

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ var io = require("socket.io")({
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 
+var http = require("http");
+
+http.createServer(function(req, res){
+	res.end("TEST succesfful");
+}).listen(server_port);
+
 io.attach(server_port);
 
 console.log("succesfful");
